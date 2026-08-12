4d12442f5aeb1fa01b401366874a46e595cf0a9e
Maheswara VULAVALA <vulavalamaheshreddy96@gmail.com>
2026-08-12 12:22:25 +0000

    cc

---
+++ models/emp_customer/customer_age_group.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='ephemeral') }} 
+
+SELECT
+    customer_id,
+    customer_name,
+    city,
+    created_date,
+    updated_at,
+    age,
+     case 
+     when age <18 then 'below 18'
+     when age between 18 and 30 then '18-30'
+     when age between 30 and 40 then '30-40'
+     when age between 40 and 50 then '40-50'
+     else '++50' end as age_group
+FROM {{ ref('stg_customers') }}
