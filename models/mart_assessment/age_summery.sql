9f58e3361ac14faa7d4211835ee419234e6565db
Maheswara VULAVALA <vulavalamaheshreddy96@gmail.com>
2026-08-12 12:48:38 +0000

    c

--- models/mart_assessment/age_summery.sql
+++ models/mart_assessment/age_summery.sql
@@ -1,6 +1,11 @@
 {{config(materialized='table')}}
 
 select 
+   customer_id,
+    customer_name,
+    age,
 age_group,
 count(*) as emp 
-from {{ ref('customer_age_group')}} group by age_group
+from {{ ref('customer_age_group')}} group by customer_id,
+    customer_name,
+    age,age_group
