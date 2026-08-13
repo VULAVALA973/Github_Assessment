85e3cb3ebc29de3632efea75e1c8a4927d9bdbb5
Maheswara VULAVALA <vulavalamaheshreddy96@gmail.com>
2026-08-13 01:49:57 +0000

    vv

---
+++ snapshots/customer_snapshot.sql
@@ -0,0 +1,26 @@
+{% snapshot customers_snapshot %} 
+
+ 
+
+{{ 
+
+config( 
+target_schema='snapshots', 
+ unique_key='customer_id', 
+strategy='timestamp', 
+updated_at='updated_date' 
+ ) 
+
+}} 
+
+ 
+
+select * 
+
+from {{ source('ASSESSMENT', 'CUSTOMERS') }} 
+
+ 
+
+{% endsnapshot %} 
+
+ 
