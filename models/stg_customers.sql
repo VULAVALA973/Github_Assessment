9b686cba90999aa98093fffa127e50a0aee5536b
Maheswara VULAVALA <vulavalamaheshreddy96@gmail.com>
2026-08-16 13:40:22 +0000

    s

--- models/stg_customers.sql
+++ models/stg_customers.sql
@@ -1,5 +1,9 @@
-SELECT
-    CUSTOMER_ID,
-    CUSTOMER_NAME,
-    CITY
-FROM {{ source('ASSESSMENT_DBT', 'CUSTOMERS1') }}
+SELECT 
+
+    customer_id, 
+
+    {{ to_upper('customer_name') }} AS customer_name 
+
+FROM {{ ref('stg_customers') }} 
+
+ 
