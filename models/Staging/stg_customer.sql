713582bfa8925debf0b9c9debc5af3f2fa159510
Maheswara VULAVALA <vulavalamaheshreddy96@gmail.com>
2026-08-18 11:06:03 +0000

    t14

--- models/Staging/stg_customer.sql
+++ models/Staging/stg_customer.sql
@@ -1,7 +1,7 @@
 
 {{config(materialized='view')}}
 
-select customer_id , customer_name,city 
+select customer_id , customer_name,city,age, created_at,updated_at
 from {{source('ASSESSMENT1','stg_customer')}}
 
 
