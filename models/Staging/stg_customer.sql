f395ee3982a46423816bd54423abd2ae6a229488
Maheswara VULAVALA <vulavalamaheshreddy96@gmail.com>
2026-08-18 12:14:43 +0000

    dsdas

--- models/Staging/stg_customer.sql
+++ models/Staging/stg_customer.sql
@@ -1,14 +1,12 @@
-{{ config(materialized='ephemeral') }} 
+{{config(materialized='table')}}
  
-SELECT
-    customer_id,
+select 
+   customer_id,
     customer_name,
     age,
-     case 
-     when age <18 then 'below 18'
-     when age between 18 and 30 then '18-30'
-     when age between 30 and 40 then '30-40'
-     when age between 40 and 50 then '40-50'
-     else '++50' 
-     end as age_group
-FROM {{ ref('stg_customer') }}
+age_group,
+count(*) as emp 
+ from {{source('ASSESSMENT1','customer')}}group by customer_id,
+    customer_name,
+    age,age_group
+    
